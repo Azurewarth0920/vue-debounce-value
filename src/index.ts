ab584371c0b68f2bb28d8d4b4cb31216eab3eb47
樊沛 <fanpei920@gmail.com>
2020-01-19 23:26:18 +0900

    added lodash debounce

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-export const foo = 'bar'
+import _Vue from 'vue'
+import debounce from 'lodash.debounce'
+
+export default function (Vue: typeof _Vue) {
+  Vue.directive('debounce-model', {
+    inserted(el, binding, vnode) {
+      console.log(vnode)
+
+      const _applyDebounce = debounce(function () {
+        if (vnode.context) {
+          vnode.context.$data[binding.expression] = (<HTMLInputElement>el).value
+        }
+      }, 200)
+
+      vnode.context[`$_applyDebounce_${binding.expression}`] = _applyDebounce
+    },
+    update (el, binding, vnode) {
+      vnode.context[`$_applyDebounce_${binding.expression}`]()
+    },
+    unbind(el, binding, vnode) {
+      vnode.context[`$_applyDebounce_${binding.expression}`].cancel
+    }
+  })
+}
